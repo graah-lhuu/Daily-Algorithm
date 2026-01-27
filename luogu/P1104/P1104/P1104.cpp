10d0219bf481fcdfcaadf508016ed53e694b631c
TAO XIA <15168493961@163.com>
2026-01-27 13:17:47 +0800

    Add files via upload

---
+++ luogu/P1104/P1104/P1104.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct Person {
+	int id;
+	string name;
+	int year;
+	int month;
+	int day;
+};
+
+bool cmp(Person &a, Person &b) {
+	if (a.year != b.year) return a.year < b.year;
+	if (a.month != b.month) return a.month < b.month;
+	if (a.day != b.day) return a.day < b.day;
+	return a.id > b.id;
+
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+	Person* p = new Person[n];
+
+	for (int i = 0; i < n; i++) {
+		p[i].id = i;
+		cin >> p[i].name >> p[i].year >> p[i].month >> p[i].day; 
+	}
+
+	sort(p, p + n, cmp);
+
+	for (int i = 0; i < n; i++) {
+		cout << p[i].name/* << " " << p[i].year << " " << p[i].month << " " << p[i].day */<< endl;
+	}
+
+	delete[] p;
+	return 0;
+
+}
