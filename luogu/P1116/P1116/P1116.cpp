b4a7e0750148f7c6fb901c29fd052a7904863e7e
TAO XIA <15168493961@163.com>
2026-01-27 21:15:01 +0800

    Add files via upload

---
+++ luogu/P1116/P1116/P1116.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	int count = 0;
+	for(int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] > arr[j]) {
+				count++;
+			}
+		}
+	}
+	cout << count;
+
+	delete[] arr;
+	return 0;
+}
