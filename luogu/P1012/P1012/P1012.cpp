9b3ea32f0a3e79ef0a46680beca237da66ab0b41
TAO XIA <15168493961@163.com>
2026-01-27 21:13:17 +0800

    Add files via upload

---
+++ luogu/P1012/P1012/P1012.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+struct Num {
+	string value;
+};
+
+bool cmp(Num& a, Num& b) {
+	return (a.value + b.value) >= (b.value + a.value);
+
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+	Num* num = new Num[n];
+	
+	for (int i = 0; i < n; i++) {
+		cin >> num[i].value;
+	}
+
+	sort(num, num + n, cmp);
+	
+	for (int i = 0; i < n; i++) {
+		cout << num[i].value;
+	}
+	delete[] num;
+	return 0;
+}
