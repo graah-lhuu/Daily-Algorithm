b8ed45c1c4109bb7127f1997236fd703b422d91b
TAO XIA <15168493961@163.com>
2026-01-27 21:16:26 +0800

    Add files via upload

---
+++ luogu/P2676/P2676/P2676.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+int main(void) {
+	long long int n, b;
+	cin >> n >> b;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n,cmp);
+	long long int sum = 0;
+	int i = 0;
+	while (sum < b) {
+		sum += arr[i];
+		i++;
+	}
+	cout << i << endl;
+
+	return 0;
+}
