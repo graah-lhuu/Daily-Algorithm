d92c06478bdc30b6e23259cee3723ea06da9d647
TAO XIA <15168493961@163.com>
2026-01-27 21:14:01 +0800

    Add files via upload

---
+++ luogu/P1068/P1068/P1068.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+struct Man {
+	int id;
+	int score;
+};
+
+bool cmp(Man& a,Man& b) {
+	if (a.score != b.score) return a.score > b.score;
+	return a.id < b.id;
+}
+
+int main(void) {
+	int n, m;
+	cin >> n >> m;
+	Man* man = new Man[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> man[i].id >> man[i].score;
+	}
+
+	sort(man, man + n, cmp);
+
+	int count = (int)floor(m * 1.5);
+	
+	while (man[count - 1].score == man[count].score) {
+		count++;
+	}
+
+	cout << man[count-1].score << " " << count << endl;
+	for (int i = 0; i < count; i++) {
+		cout << man[i].id << " " << man[i].score << endl;
+	}
+
+	delete[] man;
+
+	return 0;
+}
