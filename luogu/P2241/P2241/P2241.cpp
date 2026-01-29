5132aead3e486b299fad91ac4000063c680a9fb0
TAO XIA <15168493961@163.com>
2026-01-29 19:43:35 +0800

    Add files via upload

---
+++ luogu/P2241/P2241/P2241.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	long long int n, m;
+	cin >> n >> m;
+	long long int square, rectangle;
+	rectangle = (m + 1) * m /2 * (n + 1) * n / 2;
+
+	if (n < m) swap(n, m);//Make sure that n>=m
+	square = 0;
+	for (int i = 1; i <= m; i++) {
+		square += (m - i + 1) * (n - i + 1);
+	}
+	rectangle -= square;
+
+	cout << square << " " << rectangle << endl;
+
+	return 0;
+}
